bffd39f76ca6ec165a616367e497047d28d9d293
Artem <guts@Artems-MacBook-Air.local>
2019-11-15 12:40:09 -0800

    added get and http requests

--- src/App.js
+++ src/App.js
@@ -10,12 +10,25 @@ import axios from "axios";
 //if classes don't have state they don't need to be classes, functions also don't need
 //render is lyifecyle method
 class App extends Component {
+	//there will be a moment in time where loading is true, not false,
+	//we need to wait for that moment to pass to inform the user, that it's still loading
+	state = {
+		users: [],
+		loading: false
+	};
 	//will fire as soon as the app componet mounts,axios, res is resolve
 	//always make http respects here b=cus it will fire when the app loads
-	componentDidMount() {
-		axios
-			.get("http://api.github.com/users")
-			.then((res) => console.log(res.data));
+	async componentDidMount() {
+		this.setState({ loading: true });
+
+		const res = await axios.get("http://api.github.com/users");
+		console.log(res.data);
+		//with async await
+		//axios, without asynic aswait, without the then promise
+		// .get("http://api.github.com/users")
+		// .then((res) => console.log(res.data));
+		//after the call is made we can set loading back to false.
+		this.setState({ user: res.data, loading: false });
 	}
 	render() {
 		//const name = "Hell";
